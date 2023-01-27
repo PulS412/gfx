a965cd43f9351dbca5fdb4979f947da7439c7b8a
Brandon Klopp <brandon.klopp91@gmail.com>
2023-01-27 18:16:50 -0500

    added frame buffer callback and process input,  functions as well as window hints.

--- main.cpp
+++ main.cpp
@@ -7,17 +7,57 @@
 
 #include <iostream>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+//window dimensions
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
 int main() {
+    //initialize the glfw library
     glfwInit();
 
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    //window hints are settings that can be used to configure various
+    //aspects of a window such as its size, position, and behavior before
+    //creating the window and will affect how the window behaves.
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    //glfw window creation
     GLFWwindow* window = glfwCreateWindow(800, 600, "GLFW window", nullptr, nullptr);
+    
+    if(window == NULL){
+        std::cout << "Failed to vreate GLFW window\n";
+        glfwTerminate();
+        return -1;
+    }
 
-    uint32_t extensionCount = 0;;
+    //Make the context of the window the main context of the current thread
+    //to allow access and use of the context's data and state for rendering
+    glfwMakeContextCurrent(window);
 
-    std::cout << extensionCount << " extensions supported\n";
+    //this sets the callback function that is called when the user resizes the window
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    //Load all OpenGL function pointers to allow access to the functions defined in
+    //the OpenGL library for rendering
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+        std::cout << "Failed to initialize glad\n";
+        return -1;
+    }
 
     while(!glfwWindowShouldClose(window)) {
+        processInput(window);
+
+        //swap buffers to prevent flickering that may arise as a result of
+        //writing to the front buffer. Writing should always be done to the
+        //back buffer while the front buffer is displayed
+        glfwSwapBuffers(window);
+
+        //check for any events that may be triggered
+        //will update the window state and call callback functions
         glfwPollEvents();
     }
 
@@ -26,4 +66,19 @@ int main() {
     glfwTerminate();
 
     return 0;
+}
+
+//query glfw whether relevant keys are pressed or released in this fream and react accordingly
+void processInput(GLFWwindow* window){
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height){
+    
+    //set the dimensions of the rendering area within the window
+    //this sets up the transformation that maps the normalized
+    //device coordinates of the rendered scene to the screen
+    //coordinates of the viewport in pixels
+    glViewport(0, 0, width, height);
 }
