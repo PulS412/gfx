eb6f80bed1d9f76fda52ce66198127ddccbe438d
Brandon Klopp <brandon.klopp91@gmail.com>
2023-01-28 14:39:45 -0500

    added linking error check

--- main.cpp
+++ main.cpp
@@ -121,6 +121,15 @@ int main() {
     glAttachShader(shaderProgram, vertexShader);
     glAttachShader(shaderProgram, fragmentShader);
     glLinkProgram(shaderProgram);
+    
+    // check for linking errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
 
     while(!glfwWindowShouldClose(window)) {
         processInput(window);
