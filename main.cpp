7bca523dee2e02dce3bb13c27b29f272e1eefb5d
Brandon Klopp <brandon.klopp91@gmail.com>
2023-01-28 12:26:11 -0500

    successfully rendered an orange triangle.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,25 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
 
+// This vertex shader takes in the 3D coordinates of an object's
+//vertices and applies mathematical operations to transform them
+//into 2D screen coordinates, which are then passed to the fragment
+//shader to determine the color of each pixel on the screen
 const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
     "void main()\n"
     "{\n"
     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+// This is a simple fragment shader written in GLSL version 3.30
+//that sets the final color of a fragment to a shade of orange
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
     "}\0";
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -52,6 +63,65 @@ int main() {
         return -1;
     }
 
+    float verticies[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f
+    };
+
+    //create a vertex buffer object
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    //bind the buffer to the gl array buffer target
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    //copy the vertex data into the buffer's memory
+    glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
+
+    //create a vertex array object
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+
+    //bind vertex array object
+    glBindVertexArray(VAO);
+    //copy our verticies array in a buffer for opengl to use
+    glBindBuffer(GL_ARRAY_BUFFER, VAO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
+    //set our vertex attributes pointers
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+
+    //compile shaders
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+    glCompileShader(vertexShader);
+
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+    if(!success){
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+    glCompileShader(fragmentShader);
+
+    if(!success){
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
     while(!glfwWindowShouldClose(window)) {
         processInput(window);
 
@@ -60,33 +130,10 @@ int main() {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f); //state setting function
         glClear(GL_COLOR_BUFFER_BIT); //state using function
 
-        float verticies[] = {
-            -0.5f, -0.5f, 0.0f,
-            0.5f, -0.5f, 0.0f,
-            0.0f, 0.5f, 0.0f
-        };
-
-        //create a vertex buffer object
-        unsigned int VBO;
-        glGenBuffers(1, &VBO);
-        //bind the buffer to the gl array buffer target
-        glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        //copy the vertex data into the buffer's memory
-        glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
-
-        unsigned int vertexShader;
-        vertexShader = glCreateShader(GL_VERTEX_SHADER);
-        glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
-        glCompileShader(vertexShader);
-
-        int success;
-        char infoLog[512];
-        glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-
-        if(!success){
-            glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-            std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-        }
+        //draw the object
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         //swap buffers to prevent flickering that may arise as a result of
         //writing to the front buffer. Writing should always be done to the
@@ -99,9 +146,9 @@ int main() {
     }
 
     glfwDestroyWindow(window);
-
     glfwTerminate();
-
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
     return 0;
 }
 
